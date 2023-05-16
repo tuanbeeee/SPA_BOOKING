74a49059a2a241273b9337332eb4733a5a49d4f0
tuanbeeee <tuanblep0298@gmail.com>
2023-05-16 23:31:41 +0700

    update

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -15,15 +15,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddIdentity<Account, IdentityRole>()
-    .AddEntityFrameworkStores<SpaBookingDBContext>()
-    .AddDefaultTokenProviders();
+
 
 builder.Services.AddDbContext<SpaBookingDBContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddIdentity<Account, IdentityRole>()
+    .AddEntityFrameworkStores<SpaBookingDBContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddAuthentication(options =>
 {

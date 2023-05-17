3534a0d1408b5bdb176fd2f8498a1772299bd2c5
tuanbeeee <tuanblep0298@gmail.com>
2023-05-18 01:42:21 +0700

    update migration

--- BussinessObject/Migrations/20230517184101_UpdateTableName.Designer.cs
+++ BussinessObject/Migrations/20230517184101_UpdateTableName.Designer.cs
@@ -4,6 +4,7 @@ using BussinessObject.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BussinessObject.Migrations
 {
     [DbContext(typeof(SpaBookingDBContext))]
-    partial class SpaBookingDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230517184101_UpdateTableName")]
+    partial class UpdateTableName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

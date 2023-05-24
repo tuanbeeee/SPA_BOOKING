a95ed18d35782805e4c76e99de7163f1b9185ab4
Đoàn Trần Đức <doantranduc01@gmail.com>
2023-05-24 14:59:29 +0700

    add discount repository

---
+++ BussinessObject/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IDiscountRepository : IGenericRepository<Discount>
+    {
+        public Task<ICollection<Discount>> GetAllDiscounts();
+    }
+}

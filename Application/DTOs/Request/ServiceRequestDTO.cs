8e3b8314117465f500d43700e30d914f51ad8f7a
Đoàn Trần Đức <doantranduc01@gmail.com>
2023-05-24 15:50:40 +0700

    add service repository

---
+++ Application/DTOs/Request/ServiceRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Request
+{
+    public class ServiceRequestDTO
+    {
+        public string serviceName { get; set; }
+        public string serviceDuration { get; set; }
+        public Double servicePrice { get; set; }
+        public long spaId { get; set; }
+        
+    }
+}
---
+++ Infrastructure/Repositories/ServiceRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ServiceRepository : GenericRepository<Service>,IServiceRepository
+    {
+        private readonly SpaBookingDBContext _context;
+        public ServiceRepository(SpaBookingDBContext context):base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Service>> GetAllServices()
+        {
+            return await _context.Service
+                .Include(s => s.Discounts)
+                .Include(s=>s.Spa)
+                .ToListAsync();
+        }
+    }
+}
